44f1a4f7a03302cae8a8bf3ea69331f9184fe171
Aditya Pradana <dipta.aryadana@gmail.com>
2024-06-30 05:50:25 +0700

    test borrow exception

--- tests/unit/test_member.py
+++ tests/unit/test_member.py
@@ -8,17 +8,41 @@ import uuid
 
 class TestMemberAsync(TestsAsync):
     def setUp(self) -> None:
+        self.db = SessionLocalAppTest()
         return super().setUp()
 
     def tearDown(self) -> None:
         # self.loop.close()
+        self.db.close()
         return super().tearDown()
 
     def test_search_member(self):
-        db = SessionLocalAppTest()
-        result = service.search_member(db, filters=MemberSearch())
+        # db = SessionLocalAppTest()
+        result = service.search_member(self.db, filters=MemberSearch())
         self.assertGreater(len(result[0]), 0)
         for member in result[0]:
             self.assertIsInstance(member, Member)
 
-        db.close()
+        # db.close()
+
+    def test_borrow_book_member_not_found(self):
+        # test book not exist
+        with self.assertRaises(Exception) as e:
+            result = service.borrows(
+                self.db, member_code='M001', book_code='XXX-1')
+        self.assertIsInstance(e.exception, HTTPException)
+        self.assertEqual(404, e.exception.status_code)
+
+        # test member not exist
+        with self.assertRaises(Exception) as e:
+            result = service.borrows(
+                self.db, member_code='M005', book_code='JK-45')
+        self.assertIsInstance(e.exception, HTTPException)
+        self.assertEqual(404, e.exception.status_code)
+
+        # both not exist
+        with self.assertRaises(Exception) as e:
+            result = service.borrows(
+                self.db, member_code='M005', book_code='XXX-1')
+        self.assertIsInstance(e.exception, HTTPException)
+        self.assertEqual(404, e.exception.status_code)
